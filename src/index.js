a99d2acb4928b6ec088d8ad1568b6121fcd549cd
Kintesh <i.am@kinte.sh>
2017-08-19 23:43:29 +0100

    Use react-router-redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
-import { BrowserRouter } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
 
 import Routes from './routes';
+import createStore from './store';
+
+const history = createHistory();
+const store = createStore(history);
 
 const rootEl = document.getElementById('root');
 const render = Component =>
   ReactDOM.render(
     <AppContainer>
-      <BrowserRouter>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
           <Routes />
-      </BrowserRouter>
+        </ConnectedRouter>
+      </Provider>
     </AppContainer>,
     rootEl,
   );
---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { routerReducer, routerMiddleware } from 'react-router-redux';
+
+export default (history) => {
+  const middleware = routerMiddleware(history);
+
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(
+    combineReducers({
+      // ...rootReducer,
+      router: routerReducer
+    }),
+    composeEnhancers(applyMiddleware(middleware)),
+  );
+
+  // if (module.hot) {
+  //   module.hot.accept('./reducers', () => {
+  //     const nextReducers = require('../reducers').default;
+  //     const rootReducer = combineReducers({
+  //       ...nextReducers,
+  //       router: routerReducer
+  //     });
+  //
+  //     store.replaceReducer(rootReducer);
+  //   });
+  // }
+
+  return store;
+}
