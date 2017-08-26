0b02741c0b0bfb36b5aec71da6886ed30ed01218
Kintesh <i.am@kinte.sh>
2017-08-26 15:22:50 +0100

    Remove index.html

--- server/Renderer.js
+++ server/Renderer.js
@@ -29,8 +29,9 @@ const Renderer = (req, res) => {
   const html = renderToString(
     <html>
     <head>
-      <meta charSet="utf-8"/>
+      <meta charSet="utf-8" />
       <title>kinte.sh</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </head>
     <body>
     <script dangerouslySetInnerHTML={{__html: initialState}} />
