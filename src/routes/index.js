f78bad73810fe9d907820482517227a9457f4a8a
Kintesh <i.am@kinte.sh>
2018-02-07 22:59:24 +0000

    code splitting.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,38 @@ import { Route, Redirect } from 'react-router';
 import { Switch } from 'react-router-dom';
 
 import AppContainer from '../containers/AppContainer';
-import Home from '../components/Home';
-import Foo from '../components/Foo';
-import Bar from '../components/Bar';
+// import Home from '../components/Home';
+// import Foo from '../components/Foo';
+// import Bar from '../components/Bar';
+
+const asyncRoute = (getComponent) => {
+  return class AsyncComponent extends React.Component {
+    state = {
+      Component: null
+    };
+
+    componentDidMount() {
+      if (this.state.Component === null) {
+        getComponent().then((Component) => {
+          this.setState({Component: Component});
+        });
+      }
+    }
+
+    render() {
+      const {Component} = this.state;
+      if (Component && Component.default) {
+        return <Component.default {...this.props} />;
+      } else {
+        return <div>loading...</div>;
+      }
+    }
+  };
+};
+
+const Home = asyncRoute(() => import('../components/Home'));
+const Foo = asyncRoute(() => import('../components/Foo'));
+const Bar = asyncRoute(() => import('../components/Bar'));
 
 const Routes = () => {
   return (
